59c4f84b71428e90c82b1295e02be2e5ea7e81d8
AbdELRahmanFarhan <abdofarhan75@gmail.com>
2025-04-27 23:13:15 +0200

    visualize the dataset

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM gocv/opencv:4.11.0-static
+
+RUN apt-get update && apt-get install --no-install-recommends -y \
+    libegl1 \
+    libgl1 \
+    libgomp1 \
+    python3-pip \
+    && rm -rf /var/lib/apt/lists/*
+
+
+RUN python3 -m pip install --no-cache-dir --upgrade pip && \
+    python3 -m pip install --no-cache-dir --upgrade open3d opencv-python
+
+WORKDIR /home
+
+COPY ./src /home/src
+COPY /data /home/data
+    
+CMD ["bash"]
---
+++ src/visualize_dataset.py
@@ -0,0 +1,34 @@
+import cv2
+import open3d as o3d
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+color_np = np.load("../data/original/one-box.color.npdata.npy")
+depth_np = np.load("../data/original/one-box.depth.npdata.npy")
+
+color_np = np.uint8(color_np)
+depth_np = np.float32(depth_np)
+cv2.imwrite("../data/test/color.png", color_np)
+plt.imsave("../data/test/depth.png", depth_np, vmax=3.0, vmin=0, cmap='grey')
+
+h = depth_np.shape[0]
+w = depth_np.shape[1]
+
+intrinsic_mat = np.load("../data/original/intrinsics.npy").astype(np.float32)
+print(intrinsic_mat)
+extrinsic_mat = np.load("../data/original/extrinsics.npy").astype(np.float32)
+print(extrinsic_mat)
+
+
+color_o3d = o3d.geometry.Image(np.repeat(color_np[:, :, np.newaxis], 3, axis=2))
+
+depth_o3d = o3d.geometry.Image(depth_np*1000)
+
+rgbd_o3d = o3d.geometry.RGBDImage.create_from_color_and_depth(color_o3d, depth_o3d)
+
+intrisic_o3d = o3d.camera.PinholeCameraIntrinsic(width=w, height=h, intrinsic_matrix=intrinsic_mat)
+
+pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_o3d, intrisic_o3d, extrinsic_mat)
+
+o3d.io.write_point_cloud("../data/test/pointcloud.pcd", pcd)
